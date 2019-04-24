48fe42c5c1863235fb647f40484691711af4d077
Jake Sichley <39101429+JakeSichley@users.noreply.github.com>
2019-04-24 13:56:17 -0700

    Update String_charAt.s

--- String_charAt.s
+++ String_charAt.s
@@ -7,7 +7,7 @@
 @
 @	Returned Register Contents:
 @	R0: Character at the specified index
-@	All Register contents are preserved R0 - R3
+@	All Register contents are preserved except R0 - R3
 .data
 iStrLenA:		.word 0
 iStart:			.word 0
